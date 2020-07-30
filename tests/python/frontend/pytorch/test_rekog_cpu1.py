a8decf74b9deb07698a522153cc4d4c156d90140
alexwong <11878166+alexwong@users.noreply.github.com>
2020-07-30 10:35:52 -0700

    Commit to rebase

--- tests/python/frontend/pytorch/test_rekog_cpu1.py
+++ tests/python/frontend/pytorch/test_rekog_cpu1.py
@@ -1,6 +1,11 @@
 import torch
 from torchvision.ops import roi_align, nms
+import tvm
+
 from tvm import relay
+from tvm.contrib import graph_runtime
+from tvm.contrib.nvcc import have_fp16
+from tvm.relay.testing.config import ctx_list
 
 #filename = 'detection_model_vision_nms_800x450_cpu.pth'
 #filename = 'det_COCO_e2e_mask_rcnn_DLA34_16_2e-2_90K_tvis_nms_1312x640_cpu.pth'
@@ -38,12 +43,40 @@ print('ts nodes end')
 
 #11204264478797508617_2534701978479294800_2.pt
 
-input_name = 'input0'
+#input_name = 'input0'
 #input_shape = [1, 3, 450, 800]
 #input_shape = [3,640,1312]
-input_shape = [3,480,960]
-input_data = torch.randn(input_shape)
-shape_list = [(input_name, input_data.shape)]
-mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
+ishapes = [(3,480,960)]
+#input_data = torch.randn(input_shape)
+#shape_list = [(input_name, input_data.shape)]
+
+input_names = ["i{}".format(idx) for idx, ish in enumerate(ishapes)]
+input_shapes = list(zip(input_names, ishapes))
+
+inputs = [torch.randn(shape, dtype=torch.float)
+          for shape in ishapes]
+
+mod, params = relay.frontend.from_pytorch(scripted_model, input_shapes)
+
+#Should save or something so I don't have to wait to parse each time...
 
 #print(mod)
+
+executor = relay.create_executor("vm", mod=mod, ctx=tvm.cpu(0),
+                                 target="llvm")
+evaluator = executor.evaluate()
+
+for name, inp in zip(input_names, inputs):
+    params[name] = inp.numpy()
+
+op_res = evaluator(**params)
+
+with torch.no_grad():
+    pt_result = pt_model(*inputs)
+
+if not isinstance(pt_result, torch.Tensor):
+    tvm_res = op_res.asnumpy().item()
+    assert pt_result == tvm_res
+else:
+    tvm.testing.assert_allclose(op_res.asnumpy(), pt_result.numpy(),
+                                rtol=1e-5, atol=1e-5)
--- tests/python/frontend/pytorch/test_rekog_cpu2.py
+++ tests/python/frontend/pytorch/test_rekog_cpu2.py
@@ -1,6 +1,11 @@
 import torch
 from torchvision.ops import roi_align, nms
+import tvm
+
 from tvm import relay
+from tvm.contrib import graph_runtime
+from tvm.contrib.nvcc import have_fp16
+from tvm.relay.testing.config import ctx_list
 
 #filename = 'detection_model_vision_nms_800x450_cpu.pth'
 #filename = 'det_COCO_e2e_mask_rcnn_DLA34_16_2e-2_90K_tvis_nms_1312x640_cpu.pth'
@@ -38,12 +43,31 @@ print('ts nodes end')
 
 #11204264478797508617_2534701978479294800_2.pt
 
-input_name = 'input0'
+#input_name = 'input0'
 #input_shape = [1, 3, 450, 800]
 #input_shape = [3,640,1312]
-input_shape = [3,480,960]
-input_data = torch.randn(input_shape)
-shape_list = [(input_name, input_data.shape)]
-mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
+ishapes = [(3,480,960)]
+#input_data = torch.randn(input_shape)
+#shape_list = [(input_name, input_data.shape)]
+
+input_names = ["i{}".format(idx) for idx, ish in enumerate(ishapes)]
+input_shapes = list(zip(input_names, ishapes))
+
+inputs = [torch.randn(shape, dtype=torch.float)
+          for shape in ishapes]
+
+mod, params = relay.frontend.from_pytorch(scripted_model, input_shapes)
+
+#Should save or something so I don't have to wait to parse each time...
 
 #print(mod)
+
+with tvm.transform.PassContext(opt_level=3, disabled_pass=None):
+    vm_exec = relay.vm.compile(mod, target="llvm", params=params)
+from tvm.runtime.vm import VirtualMachine
+vm = VirtualMachine(vm_exec)
+vm.init(tvm.cpu())
+inputs1 = {}
+for e, i in zip(input_names, inputs):
+    inputs1[e] = tvm.nd.array(i)
+result = vm.invoke("main", **inputs1)
